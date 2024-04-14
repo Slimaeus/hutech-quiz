1254c30ed29242c726825e335a87c530aa570c25
Slimaeus <nguyenhongthai28042002@gmail.com>
2024-04-14 19:37:07 +0700

    fix: Comment some console log

--- dist/src/modules/http/generate.api.controller.js
+++ dist/src/modules/http/generate.api.controller.js
@@ -24,72 +24,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const generative_ai_1 = require("@google/generative-ai");
 const routing_controllers_1 = require("routing-controllers");
 const typedi_1 = require("typedi");
-const quizzes_service_1 = require("../../libs/services/quizzes.service");
 let GenerateController = class GenerateController {
-    constructor(quizzesService) {
-        this.quizzesService = quizzesService;
-    }
-    getMessage(topic) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const genAI = new generative_ai_1.GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-            const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-            const quizCount = 2;
-            const answerCount = 4;
-            const correctAnswerText = 1;
-            const prompt = `Sinh cho tôi chính xác ${quizCount} câu đố trắc nghiệm (quiz) về ${topic} có chính xác ${answerCount} câu trả lời cho mỗi câu đố với đúng ${correctAnswerText} câu trả lời đúng trong mỗi câu.
-    Cũng như tạo cho tôi một câu giải thích (explaination) hợp lý cho đáp án đúng.
-    Nhưng bạn cần phải trả lời dưới dạng 1 JSON của câu đố! Cũng KHÔNG được dùng json markdown tức không bao giờ được có chuỗi này ""\`\`\`"" cũng như không bắt đầu bằng ""\`\`\`json"". Nếu chỉ có 1 câu đố vẫn phải để trong mảng!
-    Ví dụ:
-    [
-        {{
-            ""content"": ""Nội dung câu đố"",
-            ""explaination"": ""Giải thích tại sao các đáp án lại đúng"",
-            ""answers"": [
-                {{
-                    ""content"": ""Câu trả lời 1"",
-                    ""isCorrect"": true
-                }},
-                {{
-                    ""content"": ""Câu trả lời 2"",
-                    ""isCorrect"": false
-                }},
-            ]
-        }}
-    ]`;
-            const result = yield model.generateContent(prompt);
-            const response = result.response;
-            let text = response.text();
-            console.log("Raw text");
-            console.log(text);
-            if (text.startsWith("```")) {
-                text = text.substring(3);
-            }
-            if (text.startsWith("json")) {
-                text = text.substring(4);
-            }
-            if (text.endsWith("```")) {
-                text = text.substring(0, text.length - 3);
-            }
-            const quizzes = JSON.parse(text);
-            // quizzes.forEach(async (e) => {
-            //   const formValues = new QuizFormValues();
-            //   formValues.content = e.content;
-            //   formValues.explaination = e.explaination;
-            //   formValues.score = 1;
-            //   formValues.answers = e.answers;
-            //   await this.quizzesService.create(formValues);
-            // });
-            console.log("Quizzes");
-            console.log(quizzes);
-            return quizzes;
-        });
-    }
     postMessage(topic, quizCount = 1, answerCount = 4, correctAnswerText = 4) {
         return __awaiter(this, void 0, void 0, function* () {
-            console.log(topic);
-            console.log(quizCount);
-            console.log(answerCount);
-            console.log(correctAnswerText);
+            // console.log(topic);
+            // console.log(quizCount);
+            // console.log(answerCount);
+            // console.log(correctAnswerText);
             const genAI = new generative_ai_1.GoogleGenerativeAI(process.env.GEMINI_API_KEY);
             const model = genAI.getGenerativeModel({ model: "gemini-pro" });
             const prompt = `Sinh cho tôi chính xác ${quizCount} câu đố trắc nghiệm (quiz) về ${topic} có chính xác ${answerCount} câu trả lời cho mỗi câu đố với đúng ${correctAnswerText} câu trả lời đúng trong mỗi câu.
@@ -115,8 +56,8 @@ let GenerateController = class GenerateController {
             const result = yield model.generateContent(prompt);
             const response = result.response;
             let text = response.text();
-            console.log("Raw text");
-            console.log(text);
+            // console.log("Raw text");
+            // console.log(text);
             if (text.startsWith("```")) {
                 text = text.substring(3);
             }
@@ -127,20 +68,12 @@ let GenerateController = class GenerateController {
                 text = text.substring(0, text.length - 3);
             }
             const quizzes = JSON.parse(text);
-            console.log("Quizzes");
-            console.log(quizzes);
+            // console.log("Quizzes");
+            // console.log(quizzes);
             return quizzes;
         });
     }
 };
-__decorate([
-    (0, routing_controllers_1.HttpCode)(200),
-    (0, routing_controllers_1.Get)("/:topic"),
-    __param(0, (0, routing_controllers_1.Param)("topic")),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [String]),
-    __metadata("design:returntype", Promise)
-], GenerateController.prototype, "getMessage", null);
 __decorate([
     (0, routing_controllers_1.HttpCode)(200),
     (0, routing_controllers_1.Post)(),
@@ -154,7 +87,6 @@ __decorate([
 ], GenerateController.prototype, "postMessage", null);
 GenerateController = __decorate([
     (0, typedi_1.Service)(),
-    (0, routing_controllers_1.JsonController)("/api/v1/generate", { transformResponse: true }),
-    __metadata("design:paramtypes", [quizzes_service_1.QuizzesService])
+    (0, routing_controllers_1.JsonController)("/api/v1/generate", { transformResponse: true })
 ], GenerateController);
 exports.default = GenerateController;
